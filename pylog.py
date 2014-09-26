0eb236126ab250a93dde74b8cff17f0d762e0c43
augre <augre.augre@gmail.com>
2014-09-26 11:26:39 -0500

    Added timestamp

--- pylog.py
+++ pylog.py
@@ -3,12 +3,17 @@
 
 from __future__ import print_function
 import serial, io
+import datetime
+
+
 
 addr  = 16  # serial port to read data from
 baud  = 115200            # baud rate for serial port
 fname = 1   # log file to save data in
 fmode = 'a'             # log file mode = append
 flag=0
+fstart="DDR OK"
+fend="TEST FAIL"
 
 with serial.Serial(addr,baud) as pt:
     spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
@@ -18,11 +23,11 @@ with serial.Serial(addr,baud) as pt:
         with open(str(fname),fmode) as outf:
             x = spb.readline()  # read one line of text from serial port
             print (x,end='')    # echo line of text on-screen
-            outf.write(x)       # write line of text to file
+            outf.write("["+str(datetime.datetime.utcnow())+"]"+x)       # write line of text to file
             outf.flush()        # make sure it actually gets written out
-        if x.find("TEST FAIL")!=-1:
+        if x.find(fend)!=-1:
             flag=1
-        if x.find("DDR OK")!=-1 and flag==1:
+        if x.find(fstart)!=-1 and flag==1:
             fname=fname+1
             flag=0
         else:
