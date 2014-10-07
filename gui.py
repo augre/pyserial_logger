e49478fcd3e29901d6261c5285e41c22b459952b
augre <augre.augre@gmail.com>
2014-10-07 16:41:41 -0500

    def __init__(self): and wx.App.__init__(self)

--- gui.py
+++ gui.py
@@ -1,9 +1,19 @@
+# -*- coding: cp1252 -*-
 import datetime
 import wx
 
 print datetime.datetime.utcnow()
 
 class App(wx.App):
+    """
+Notice that we didn’t define an __init__() method for our application class. In
+Python, this means that the parent method, wx.App.__init__(), is automatically
+invoked on object creation. This is a good thing. If you define an __init__()
+method of your own, don’t forget to call the __init__() of the base class, like this:
+"""
+
+    def __init__(self):
+        wx.App.__init__(self)
 
     def OnInit(self):
         frame=wx.Frame(parent=None, title='Bare')
