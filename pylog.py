351b479238688ca28b6f8df34b6a7ca9fa295030
augre <augre.augre@gmail.com>
2014-11-24 17:09:52 -0600

    start on command line arguments

--- pylog.py
+++ pylog.py
@@ -3,33 +3,52 @@
 
 # get lines of text from serial port, save them to a file
 
-#this line uses print as a function like inpython 3
-from __future__ import print_function
+
 
 
 import serial, io
 import time
 import datetime
 import os
-import sys
+import sys, getopt
+
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+   except getopt.GetoptError:
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'test.py -i <inputfile> -o <outputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputfile = arg
+      elif opt in ("-o", "--ofile"):
+         outputfile = arg
+   print 'Input file is "', inputfile
+   print 'Output file is "', outputfile
 
 
 
-addr  = '/dev/ttyUSB0'  # serial port to read data from
-baud  = 115200            # baud rate for serial port
-location =os.path.normpath("/home/z3/Z3/Customers/Carmanah/0365M05144610")
-
-fname = 248   # log file to save data in, serial number
-fmode = 'a'             # log file mode = append
-flag=0
-fstart="PSP 2.10.00.08 v0.1"
-fend="# davi"
 
 
+if __name__ == "__main__":
+   main(sys.argv[1:])
+   
+   addr  = '/dev/ttyUSB0'  # serial port to read data from
+   baud  = 115200            # baud rate for serial port
+   location =os.path.normpath("/home/z3/Z3/Customers/Carmanah/0365M051446100")
+   fname = 92   # log file to save data in, serial number
+   fmode = 'a'             # log file mode = append
+   flag=0
+   fstart="PSP 2.10.00.08 v0.1"
+   fend="# davi"
 
-
-
-with serial.Serial(addr,baud) as pt:
+   with serial.Serial(addr,baud) as pt:
     spb = io.TextIOWrapper(io.BufferedRWPair(pt,pt,1),
         encoding='ascii', errors='ignore', newline='\r',line_buffering=True)
     
@@ -37,7 +56,7 @@ with serial.Serial(addr,baud) as pt:
         x = spb.readline()  # read one line of text from serial port
         if x.find(fend)!=-1:
             flag=1
-            print('\n\nFlag Set\n\n', file=sys.stderr)
+            print >> sys.stderr, '\n\nFlag Set\n\n'
             pt.write('\r')
 
             
@@ -50,7 +69,7 @@ with serial.Serial(addr,baud) as pt:
             if x.find(fstart)!=-1:
                 ts = time.time()
                 outf.write("\n\n\n["+datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')+"]\n\n\n")  # timestamp at the beginning of the file 
-            print (x,end='')    # echo line of text on-screen
+            print x    # echo line of text on-screen
 ##            outf.write("["+str(datetime.datetime.utcnow())+"]"+x)       # write line of text to file]
             outf.write(x)
             outf.flush()        # make sure it actually gets written out
